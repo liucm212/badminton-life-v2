f8d54059ed4cd1089d794bf15b2fad56721dd0eb
liucm212 <andyliu02120926@gmail.com>
2023-09-05 15:08:00 +0800

    新增forum-preventDefault.js，預防forum表單在submit後進行跳轉

---
+++ js/forum-preventDefault.js
@@ -0,0 +1,20 @@
+// 預防forum在送出表單時跳轉
+// 取得所有的.forum-forms
+let forumForms = document.querySelectorAll('.forum-form') ; 
+
+
+
+
+document.addEventListener("DOMContentLoaded", function(){
+  forumForms.forEach(function(form){
+    form.addEventListener('submit', function(e){
+      // 預防他直接跳轉
+      e.preventDefault()
+      // console.log("成功預防跳轉")
+      // 取得forum內的text input
+      let textInput = this.querySelector(".forum-input")
+      textInput.value = '' ;
+      console.log('清空input value')
+    })
+  })
+})
